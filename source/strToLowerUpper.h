27d2159fb0c69f7842d7c666813cec4a2f8ef436
Vladislav Yudin <vladislav.yudin.200@gmail.com>
2023-10-19 13:36:32 +0300

    deleted the extra files

--- source/strToLowerUpper.h
+++
@@ -1,9 +0,0 @@
-#ifndef _STR_TO_LOWER_UPPER
-#define _STR_TO_LOWER_UPPER
-
-#include <string>
-
-std::string StrToLower(std::string s);
-std::string StrToUpper(std::string s);
-
-#endif  // _STR_TO_LOWER_UPPER
